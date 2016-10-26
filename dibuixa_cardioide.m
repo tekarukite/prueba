84135676b4e765c34f7037ff6586aae5b1e9afbc
tekarukite <dostedavid@gmail.com>
2016-10-26 20:57:17 +0200

    EJERCICIO 1 CALCULO NUMERICO
    Funciona todo con graficas y todo

--- dibuixa_cardioide.m
+++ dibuixa_cardioide.m
@@ -1,7 +1,8 @@
-function f = cardioide( t, v )
+function [] = dibuixa_cardioide( v )
 
 x0 = v(1);
 y0 = v(2);
+t = -1:0.01:3;
 
 x = 2 * cos(t) - cos(2*t);
 y = 2 * sin(t) - sin(2*t);
@@ -12,8 +13,12 @@ y1 = 2*cos(t) - 2*cos(2*t);
 x2 = -2*cos(t) + 4*cos(2*t);
 y2 = -2*sin(t) + 4*sin(2*t);
 
-f = (x - x0)*x1 + (y - y0)*y1;
-f1 = x1^2 + (x-x0)*x2 + y1^2 + (y - y0)*y2;
+f = (x - x0).*x1 + (y - y0).*y1;
 
+plot(t, f);
+hold on;
+plot(t, zeros(length(t)), 'k');
+hold on;
 end
+
 
